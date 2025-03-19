9ec109cf68a62878d85b90a306364324cce39703
SNaveenMathew <naveenmathewnathan.s@gmail.com>
2025-03-18 20:49:58 -0400

    Installing windows requirements

--- 05 - Building and Evaluating Advanced RAG/windows_env_info.sh
+++ 05 - Building and Evaluating Advanced RAG/windows_env_info.sh
@@ -3,3 +3,4 @@ conda create -n dlai-short-courses2 python=3.9.18
 conda activate dlai-short-courses2
 pip install ipykernel
 python -m ipykernel install --user --name dlai-short-courses2 --display-name "Python (dlai-short-courses2)"
+pip install -r windows_requirements.txt
